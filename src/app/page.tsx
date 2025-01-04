de53e401fbce417127c8d90371907ff2977357cf
SMarkus27 <pereira.markus.27@hotmail.com>
2025-01-04 14:11:48 -0300

    chore: images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         </div>
         <div className="flex space-x-3 ">
           <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sw font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
-            Play
+            Plays
           </button>
           <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sw font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
             More Info
